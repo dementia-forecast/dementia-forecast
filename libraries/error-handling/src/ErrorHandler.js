11766f7e659bf9a304c6bbb1f95382987aef291f
Jaeeyun <nalu2696@gmail.com>
2025-05-02 05:52:05 +0900

    Chore: ErrorHandler 타입 변경

--- libraries/error-handling/src/ErrorHandler.js
+++ libraries/error-handling/src/ErrorHandler.js
@@ -1,26 +1,26 @@
 const AppError = require("./AppError");
-const logger = require("./logger"); // logger 가져오기
+const logger = require("../../logger"); // logger 가져오기
 
-class ErrorHandler {
+const ErrorHandler = {
   handleError(err, res) {
     // 에러를 로깅
     logger.error(`Error: ${err.message}\nStack: ${err.stack}`);
 
     if (err instanceof AppError) {
       // 운영 에러 처리
-      res.status(err.httpCode).json({
-        statusCode: err.httpCode,
-        message: err.description,
+      res.status(err.statusCode).json({
+        statusCode: err.statusCode,
+        message: err.message,
       });
     } else {
       // 예기치 못한 에러 처리
       console.error(err);
       res.status(500).json({
         statusCode: 500,
-        message: err.description,
+        message: err.message || "Internal Server Error",
       });
     }
-  }
-}
+  },
+};
 
 module.exports = ErrorHandler;
